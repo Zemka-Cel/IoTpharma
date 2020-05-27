680460866bedea81937e11b4b2f7e581e1d035c1
Zemka-Cel <azemina.celebic@stu.ssst.edu.ba>
2020-05-28 00:14:07 +0200

    Homescreen almost done, possible style changes

--- src/App.js
+++ src/App.js
@@ -14,19 +14,21 @@ function App() {
   return (
     <BrowserRouter>
     <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/HomeScreen" className="navbar-brand">
-          Home
-          </a>
+      <nav className="navbar navbar-expand text-white">
+      
         <div className="navbar-nav mr-auto">
-       
+        <li className="nav-item">
+            <Link to={"/HomeScreen"} className="nav-link text-white">
+            Home
+              </Link>
+          </li>
           <li className="nav-item">
-            <Link to={"/details"} className="nav-link">
+            <Link to={"/details"} className="nav-link text-white ">
               Details
               </Link>
           </li>
         </div>
-        <a href="/edit" className="navbar-brand">
+        <a href="/edit" className="nav-link text-white">
           Edit
           </a>
       </nav>
@@ -42,7 +44,11 @@ function App() {
         </Switch>
       </div>
     </div>
-
+    <nav className="navbar navbar-expand text-white" id="center">
+      
+        &copy; Copyright Zemka Diplomski
+      
+      </nav>
   </BrowserRouter>
   );
 }
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,71 +1,52 @@
-// import React from 'react';
-// import Axios from 'axios';
-
-
-
-// class HomeScreen extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             products: []
-//         };
-//     }
-    
-//     componentDidMount() {
-//         Axios.get('https://api.thingspeak.com/channels/1064445/fields/1.json?results=10')
-//             .then(response => {
-//                 this.setState({ products: response.data })
-//             })
-//     }
-    
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1 className="text-center">Welcome to <br></br> IoT pharmacy</h1>
-//                 <p  className="text-center">Proof of concept</p>
-//                 {this.state.products.map(product => (
-//                             <div>
-//                                 <p>({product.id}) </p>
-//                              </div>
-                             
-//                              ))}
-                        
-
-
-//             </div>
-//         )
-//     }
-// }
-
-// export default HomeScreen;
-
 import React from 'react';
 import Axios from 'axios';
+import '../style/HomeScreenStyle.css';
 
 
 
 class HomeScreen extends React.Component {
-    state = { data: [] };
-
-    componentDidMount() {
-      fetch('https://api.thingspeak.com/channels/1064445/fields/1.json?results=1&SBN517D6DY76WTTY')
-        .then(response => response.json())
-        .then((data) =>{
-          this.setState({data: data.field1})}
-        )
-        .catch(console.log)
+    constructor() {
+        super();
+        this.state = {
+            distance: []
+        };
     }
+    
+    componentDidMount() {
+        Axios.get('https://api.thingspeak.com/channels/1064445/fields/1.json?results=1&SBN517D6DY76WTTY')
+            .then(response => {
+                this.setState({ distance: response.data.feeds })
+            })
+    }
+    
 
     render() {
-      return (
-        <div>
-          <ul>
-            {this.state.data.map(s=>(<li>{s}</li>))}
-          </ul>
-        </div>
-      );
+        return (
+            <body>
+                <h1 className="text-center">Welcome to <br></br> IoT pharmacy</h1>
+                <p  className="text-center">Proof of concept</p>
+                <main>
+                {this.state.distance.map((d) => (
+                            <div id="distance"  className="text-center">
+                                <p><strong>Distance:</strong>  {d.field1} (cm)</p>
+                             </div>
+                             
+                             ))}
+
+{this.state.distance.map((d) => (
+                            <div id="weight" className="text-center">
+                                <p><strong>Weight:</strong> {d.field1} (gr)</p>
+                             </div>
+                             
+                             ))}
+
+</main>
+                        <div className="button mt-xl-5">
+                       <button  onClick={()=> this.props.history.push('/details')} class="btn text-white font-weight-normal lead"type="button">See the details</button>
+                       </div>
+            </body>
+        )
     }
-  }
+}
 
 export default HomeScreen;
